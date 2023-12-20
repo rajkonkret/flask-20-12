4b2517fbfedec291fd0d405ac3c6db79de22ed7f
rajkonkret <rajkonkret660@gmail.com>
2023-12-20 10:27:11 +0100

    1 - query string

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return f"<h1>Hallo World!!!</h1>"
+    print(request.query_string)
+
+    for p in request.args:
+        print(p, request.args[p])
+
+    color = 'black'
+    if 'color' in request.args:
+        color = request.args['color']
+
+    style = 'normal'
+    if 'style' in request.args:
+        style = request.args['style']
+
+    return f'<h1 style="color: {color};font-style: {style}">Hallo World!!!</h1>'
 
 
 @app.route("/about")
@@ -13,6 +26,13 @@ def about():
     a = 10
     b = 1
     return f"<h1>We are programmers {a / b}<h1>"
+
+
+# /cantor/usd/30
+@app.route('/cantor/<currency>/<int:amount>')
+def cantor(currency, amount):
+    message = f"<h1>You selected {currency} and {amount}<h1>"
+    return message
 
 
 if __name__ == '__main__':
